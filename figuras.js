3dcb4dee6d3f916ed801162648a1dd3f2d0c01b2
Product Design <design@iMac-de-Shel.localdomain>
2021-08-05 17:41:58 -0500

    Perímetro y área de triángulo

--- figuras.js
+++ figuras.js
@@ -32,3 +32,37 @@ function perimetroCuadrado (lado) {
  function areaCirculo(radio){
      return PI * (radio*radio)
  };
+
+ function calcularPerimetroCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+ }
+
+ function calcularAreaCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    const area = areaCuadrado(value);
+    alert(area);
+}
+
+function calcularPerimetroTriangulo(){
+    const input1 = document.getElementById("InputTriangulo1");
+    const input2 = document.getElementById("InputTriangulo2");
+    const inputBase = document.getElementById("InputTrianguloBase");
+    const value1 = parseFloat(input1.value)
+    const value2 = parseFloat(input2.value)
+    const base = parseFloat(inputBase.value)
+    const perimetro = perimetroTriangulo(value1, value2, base);
+    alert(perimetro);
+}
+
+function calcularAreaTriangulo(){
+    const inputAltura = document.getElementById("InputTrianguloAltura");
+    const inputBase = document.getElementById("InputTrianguloBase");
+    const altura = parseFloat(inputAltura.value);
+    const base = parseFloat(inputBase.value);
+    const area = areaTriangulo(base, altura);
+    alert(area);
+}
